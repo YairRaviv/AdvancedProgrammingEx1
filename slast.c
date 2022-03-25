65042310a122b8e50a83a8eb06ca1dfd7482e35e
liel berniker <lielb101995@gmail.com>
2022-03-25 14:56:37 +0300

    last ver

--- slast.c
+++ slast.c
@@ -1,315 +1,107 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <utmp.h>
-
 #include <ctype.h>
-
 #include <fcntl.h>
-
 #include <unistd.h>
-
 #include <string.h>
-
 #include <time.h>
 
 
-
-
-
 void show_info(struct utmp* current_user , time_t prev_time);
-
 void show_time(time_t cur_time);
 
-void calculate_time(time_t prev_time , time_t cur_time);
-
-void print_with_0(int num);
-
-
-
 int main(int argc, char * argv[])
-
 {
-
     int number;
-
     if(argc <2)
-
     {
-
-        number = 1;
-
+        return -1;
     }
-
-
 
     else
-
     {
-
         number =atoi(argv[1]);
-
     }
-
     struct utmp current_user;
-
     char* path_wtmp = "/var/log/wtmp";
-
     int fd;
-
     int size_utmp = sizeof(current_user);
-
     if ((fd = open(path_wtmp,O_RDONLY))==-1)
-
     {
-
         perror(path_wtmp);
-
         exit(1);
-
     }
+    lseek(fd,0,SEEK_SET);
+
+    int a = read(fd,&current_user,size_utmp);
+    time_t first_log = current_user.ut_tv.tv_sec;
 
     int i = 1;
-
     int counter = 0;
-
     lseek(fd,-1*(size_utmp),SEEK_END);
-
-
 
     time_t prev_time = 0;
 
-
-
     while (read(fd,&current_user,size_utmp) == size_utmp && counter<number)
-
     {
-
-        if(strcmp(current_user.ut_user,"runlevel")!= 0 && strcmp(current_user.ut_user,"shutdown")!=0) 
-
+        if(current_user.ut_type == 2 || current_user.ut_type == 7 ) 
         {
-
-
 
             show_info(&current_user , prev_time);
 
-
-
             counter++;
-
         }
-
         i++;
-
         lseek(fd,-1*(i*size_utmp),SEEK_END);
-
-
 
         prev_time = current_user.ut_tv.tv_sec;
 
-
-
     }
-
+    printf("\n");
+    char *cp;
+    cp = ctime (&first_log);
+    printf ("wtmp begins %s", cp);
     close(fd);
-
     return 0;
-
 }
-
-
-
 
 
 void show_info(struct utmp* current_user , time_t prev_time)
 
-
-
 {
-
     printf("%-8.8s",current_user->ut_user);
-
     printf(" ");
-
     if (strcmp(current_user->ut_user, "reboot") == 0)
-
     {
-
         printf("system boot ");
-
     }
-
     else
-
     {
-
         printf("%-8.8s    ",current_user->ut_line);
-
     }
-
     printf(" ");
-
     if(strcmp(current_user->ut_host, ":0") == 0 || strcmp(current_user->ut_host, ":1") == 0)
-
     {
-
         printf("%s              ", current_user->ut_host);
-
     }
 
-
-
     else
-
     {
-
         printf("%16.16s", current_user->ut_host);
-
     }
-
     printf(" ");
-
     show_time(current_user->ut_tv.tv_sec);
-
-
-
-    printf(" ");
-
-    if(prev_time == 0)
-
-    {
-
-        printf("  still logged in");
-
-    }
-
-    else
-
-    {
-
-        if (strcmp(current_user->ut_user, "reboot") == 0)
-
-        {
-
-            printf("  still running");
-
-        }
-
-        else
-
-        {
-
-            calculate_time(prev_time,current_user->ut_tv.tv_sec);
-
-        }
-
-    }
-
-
-
+    
     printf("\n");
-
-}
-
-
-
-void calculate_time(time_t prev_time , time_t cur_time)
-
-{
-
-    time_t diff =  prev_time - cur_time;
-
-    if(diff <= 86400)
-
-    {
-
-        printf("- crash  (");
-
-        int min = diff/60;
-
-        int hours = min/60;
-
-        int only_min = min - (hours*60);
-
-        print_with_0(hours);
-
-        printf(":");
-
-        print_with_0(only_min);
-
-        printf(")");
-
-    }
-
-    else
-
-    {
-
-        printf("- crash (");
-
-        int days = diff/86400;
-
-        diff = diff-(days*86400);
-
-        int min = diff/60;
-
-        int hours = min/60;
-
-        int only_min = min - (hours*60);
-
-        printf("%d+",days);
-
-        print_with_0(hours);
-
-        printf(":");
-
-        print_with_0(only_min);
-
-        printf(")");
-
-    }
-
-}
-
-void print_with_0(int num)
-
-{
-
-    if(num<10)
-
-    {
-
-        printf("%d%d",0,num);
-
-    }
-
-    else
-
-    {
-
-        printf("%d",num);
-
-    }
-
-     
-
-
-
 }
 
 void show_time(time_t cur_time)
-
 {
-
     char *cp;
-
     cp = ctime (&cur_time);
-
     cp[strlen(cp)-9] = '\0';
-
     printf ("%s", cp);
 
-
-
 }
+
